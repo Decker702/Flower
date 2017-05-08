825145431eb29550a94c34828e672006b509fac5
LDecker <Decker702@yahoo.com>
2017-05-08 14:44:46 -0400

    Need to be logged in to see Note in Nav Bar

---
+++ Flower.Models/NoteCreate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flower.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Please enter at least 1 number or character")]
+        [MaxLength(130)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(8888)]
+        public string Content { get; set; }
+
+        //This is optional
+        public string  Content1 { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+}
---
+++ Flower.Web/Controllers/NoteController.cs
@@ -0,0 +1,29 @@
+﻿using Flower.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flower.Web.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+      //      var model = new NoteListItemModel[0];
+            var model = new NoteListItem[0];
+
+            return View(model);
+        }
+
+        // CREATE: Note
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
